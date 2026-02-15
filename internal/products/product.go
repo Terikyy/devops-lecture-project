6f1636e99141dc753ef209ff2ab453267a639ced
kienle-k <kevin.k@gmx.net>
2026-02-15 09:37:55 +0100

    chore: add first products to product.go

--- internal/products/product.go
+++ internal/products/product.go
@@ -6,11 +6,12 @@ type Product struct {
 	Price float64 `json:"price"`
 }
 
-// Static data for three products
 var products = []Product{
-	{ID: 1, Name: "Product 1", Price: 19.99},
-	{ID: 2, Name: "Product 2", Price: 29.99},
-	{ID: 3, Name: "Product 3", Price: 39.99},
+	{ID: 1, Name: "Gary Slippers", Price: 29.99},
+	{ID: 2, Name: "Fish Slippers", Price: 19.99},
+	{ID: 3, Name: "Avocado Slippers", Price: 29.99},
+	{ID: 4, Name: "Croissant Slippers", Price: 19.99},
+	{ID: 5, Name: "Cat Slippers", Price: 19.99},
 }
 
 func GetProducts() []Product {
